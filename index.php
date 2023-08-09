5627207006aa126a17477ebefdf3a5dad25a5dd0
Dwijuniarto9 <dwijuniarto0506@gmail.com>
2023-08-09 15:43:06 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>HALLO WIR</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt ipsam, libero voluptate dolores vel error optio aliquam obcaecati eos placeat porro nihil suscipit animi aperiam, asperiores totam facere. Molestias, quasi.</p>
 </body>
 </html>
