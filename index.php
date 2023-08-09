3ad5dcd588908aefba4428c3d38cfc37911d3ea1
Dwijuniarto9 <dwijuniarto0506@gmail.com>
2023-08-09 15:54:35 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>HALLO WIR</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt ipsam, libero voluptate dolores vel error optio aliquam obcaecati eos placeat porro nihil suscipit animi aperiam, asperiores totam facere. Molestias, quasi.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
